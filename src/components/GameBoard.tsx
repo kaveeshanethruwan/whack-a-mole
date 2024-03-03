4c1120b667f4a03384b753182f5013ec1e6f085a
kaveeshanethruwan <kaveesha.james151@gmail.com>
2024-03-03 10:54:37 +0530

    Moved logic to a hook: useAction

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,38 +1,9 @@
 import mole from "../assets/mole.png";
 import hole from "../assets/hole.png";
-import { useEffect, useState } from "react";
+import useActions from "../hooks/useActions";
 
 const GameBoard = () => {
-  const [moles, setMoles] = useState<boolean[]>(new Array(9).fill(false));
-  const [score, setScore] = useState<number>(0);
-
-  const updateMoles = (index: number, value: boolean) => {
-    const updatedMoles = [...moles];
-    updatedMoles[index] = value;
-    setMoles(updatedMoles);
-  };
-
-  useEffect(() => {
-    let randomIndex: number;
-    const interval = setInterval(() => {
-      randomIndex = Math.floor(Math.random() * moles.length);
-      updateMoles(randomIndex, true);
-
-      setTimeout(() => {
-        updateMoles(randomIndex, false);
-      }, 800);
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [moles]);
-
-  const updateScore = (index: number) => {
-    if (!moles[index]) return;
-    updateMoles(index, false);
-    setScore(score + 1);
-  };
+  const { score, moles, updateScore } = useActions();
 
   return (
     <>
---
+++ src/hooks/useActions.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const useActions = () => {
+  const [moles, setMoles] = useState<boolean[]>(new Array(9).fill(false));
+  const [score, setScore] = useState<number>(0);
+
+  const updateMoles = (index: number, value: boolean) => {
+    const updatedMoles = [...moles];
+    updatedMoles[index] = value;
+    setMoles(updatedMoles);
+  };
+
+  useEffect(() => {
+    let randomIndex: number;
+    const interval = setInterval(() => {
+      randomIndex = Math.floor(Math.random() * moles.length);
+      updateMoles(randomIndex, true);
+
+      setTimeout(() => {
+        updateMoles(randomIndex, false);
+      }, 800);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [moles]);
+
+  const updateScore = (index: number) => {
+    if (!moles[index]) return;
+    updateMoles(index, false);
+    setScore(score + 1);
+  };
+
+  return {
+    score,
+    moles,
+    updateScore,
+  };
+};
+
+export default useActions;
